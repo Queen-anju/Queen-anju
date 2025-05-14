a703948387366b3d3fe7d66ce22d83754bc69009
Queen-anju <pukaya72@gmail.com>
2025-05-14 14:13:19 +0530

    Create Setting.js

---
+++ Setting.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=JlZ3CR5C#_XYPLx8F2xiNVwQJemt8mB0fL16rTXWEPnJbRtQ4cI4",
+POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
+};
